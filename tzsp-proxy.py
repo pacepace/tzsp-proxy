f9e6286472a56bcd1c18e183a6065d0cf0c802dc
Mark Pace <pace@illuminati.org>
2023-08-05 16:08:18 -0700

    docker configuration

--- tzsp-proxy.py
+++ tzsp-proxy.py
@@ -7,6 +7,9 @@
 # Version 1.1
 # 2021-04-02
 #
+# Modified to make it work more easily with Docker
+# 2023-08-05
+#
 
 # Script assumes that the TZSP Packets will be received on eth0. Replace on line 48 if that is different for you.
 # Script assumes that the promiscious interface is bond0. Replace this  on line 40 with the interface where you sensor is listening. 
@@ -26,7 +29,7 @@ load_dotenv()
 # tzsp receive interface
 IFACE_TZSP = os.environ.get('IFACE_TZSP', default="eth0")
 # output suricata interface
-IFACE_SURICATA = os.environ.get('IFACE_SURICATA', default="eth0")
+IFACE_SNIFFER = os.environ.get('IFACE_SNIFFER', default="eth0")
 
 # load tzsp library
 load_contrib("tzsp")
@@ -36,7 +39,7 @@ def getHwAddr(ifname):
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
     return ':'.join('%02x' % b for b in info[18:24])
 
-mac_str = str(getHwAddr("eth0"))
+mac_str = str(getHwAddr(IFACE_SNIFFER))
 
 # extract each packet received and resend it to the local interface
 # the original destination mac will be lost
@@ -47,7 +50,7 @@ def processPacketCapture ( tzspCapture ):
              rawPacket = tzspPacket[2]
              try:
                  rawPacket[Ether].dst = mac_str
-                 sendp(rawPacket, iface="eth0", verbose=False)
+                 sendp(rawPacket, iface=IFACE_SNIFFER, verbose=False)
              except:
                  print("Exception!")
                  print(repr(tzspRawPacket))
@@ -55,4 +58,4 @@ def processPacketCapture ( tzspCapture ):
              print("Exception!")
 
 # start sniffing indefinitely
-sniff(prn=processPacketCapture, iface="eth0", filter = "udp port 37008", store=0)
+sniff(prn=processPacketCapture, iface=IFACE_TZSP, filter = "udp port 37008", store=0)
