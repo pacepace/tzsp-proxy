1e2a9a0569d07d9e896065a6da5f95bdedf72022
Mark Pace <pace@illuminati.org>
2023-08-05 23:41:30 -0700

    adding sniffer verbose setting

--- tzsp-proxy.py
+++ tzsp-proxy.py
@@ -26,42 +26,11 @@ from scapy.all import *
 # load enviroment from .env
 load_dotenv()
 
-print('... tzsp proxy starting ...')
-# tzsp receive interface
-IFACE_TZSP = os.environ.get('IFACE_TZSP', default='eth0')
-# output raw packets to this sniffer interface
-IFACE_SNIFFER = os.environ.get('IFACE_SNIFFER', default='eth0')
-# verbose raw packet sending
-SNIFFER_SEND_VERBOSE = os.environ.get('SNIFFER_SEND_VERBOSE', default=False)
-SNIFFER_SEND_VERBOSE = strtobool(SNIFFER_SEND_VERBOSE)
-print (f'... IFACE_TZSP: {IFACE_TZSP} ...')
-print (f'... IFACE_SNIFFER: {IFACE_SNIFFER} ...')
-print (f'... SNIFFER_SEND_VERBOSE: {SNIFFER_SEND_VERBOSE} ...')
-
-# load tzsp library
-print('... tzsp library loading ...')
-load_contrib('tzsp')
-
-# convert whacko strings to boolean (looking at you Polycom)
-def strtobool(val):
-    """Convert a string representation of truth to true (1) or false (0).
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
-    'val' is anything else.
-    """
-    val = val.lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
-        return True
-    else:
-        return False
-
-# get the mac address of the sniffer interface
+# return the mac address of the interface
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
     return ':'.join('%02x' % b for b in info[18:24])
-
-mac_str = str(getHwAddr(IFACE_SNIFFER))
 
 # extract each packet received and resend it to the local interface
 # the original destination mac will be lost
@@ -86,6 +55,38 @@ def processPacketCapture ( tzspCapture ):
     except Exception as err:
         print(f'Capture Exception: {err}')
         pass
+
+# convert whacko strings to boolean (looking at you Polycom)
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    else:
+        return False
+
+print('... tzsp proxy starting ...')
+# tzsp receive interface
+IFACE_TZSP = os.environ.get('IFACE_TZSP', default='eth0')
+# output raw packets to this sniffer interface
+IFACE_SNIFFER = os.environ.get('IFACE_SNIFFER', default='eth0')
+# verbose raw packet sending
+SNIFFER_SEND_VERBOSE = os.environ.get('SNIFFER_SEND_VERBOSE', default=False)
+SNIFFER_SEND_VERBOSE = strtobool(SNIFFER_SEND_VERBOSE)
+print (f'... IFACE_TZSP: {IFACE_TZSP} ...')
+print (f'... IFACE_SNIFFER: {IFACE_SNIFFER} ...')
+print (f'... SNIFFER_SEND_VERBOSE: {SNIFFER_SEND_VERBOSE} ...')
+
+# load tzsp library
+print('... tzsp library loading ...')
+load_contrib('tzsp')
+
+# get the mac address of the sniffer interface
+mac_str = str(getHwAddr(IFACE_SNIFFER))
 
 print('... tzsp capturing ...')
 while True:
