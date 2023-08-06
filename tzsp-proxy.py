d0a7b43d82ca72aaebee70642671b7e2f4da3638
Mark Pace <pace@illuminati.org>
2023-08-05 23:37:26 -0700

    adding sniffer verbose setting

--- tzsp-proxy.py
+++ tzsp-proxy.py
@@ -20,6 +20,7 @@ import socket
 import struct
 import os
 
+from distutils.util.strtobool import strtobool
 from dotenv import load_dotenv
 from scapy.all import *
 
@@ -33,16 +34,29 @@ IFACE_TZSP = os.environ.get('IFACE_TZSP', default='eth0')
 IFACE_SNIFFER = os.environ.get('IFACE_SNIFFER', default='eth0')
 # verbose raw packet sending
 SNIFFER_SEND_VERBOSE = os.environ.get('SNIFFER_SEND_VERBOSE', default=False)
-if SNIFFER_SEND_VERBOSE == 'True' or SNIFFER_SEND_VERBOSE=='true' or SNIFFER_SEND_VERBOSE=='1':
-    SNIFFER_SEND_VERBOSE = True
-else:
-    SNIFFER_SEND_VERBOSE = False
+SNIFFER_SEND_VERBOSE = strtobool(SNIFFER_SEND_VERBOSE)
+print (f'... IFACE_TZSP: {IFACE_TZSP} ...')
+print (f'... IFACE_SNIFFER: {IFACE_SNIFFER} ...')
 print (f'... SNIFFER_SEND_VERBOSE: {SNIFFER_SEND_VERBOSE} ...')
 
 # load tzsp library
 print('... tzsp library loading ...')
 load_contrib('tzsp')
 
+# convert whacko strings to boolean (looking at you Polycom)
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    else:
+        return False
+
+# get the mac address of the sniffer interface
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
